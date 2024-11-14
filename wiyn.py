82e2887712a456208099e1306978aebb3443750d
dimaary <dmitry.is.so.sigma@gmail.com>
2024-11-14 15:36:11 +0800

    Update wiyn.py

--- wiyn.py
+++ wiyn.py
@@ -1,4 +1,5 @@
 name = input('what is your name ')
 age = input('how old are you ')
 location = input('where do you live ')
-print('your name is' ,name, 'your age is' ,age, 'you live in' ,location)
+hobby = input('what is your hobby ')
+print('your name is' ,name, 'your age is' ,age, 'you live in' ,location, 'your hobby is',hobby,)
